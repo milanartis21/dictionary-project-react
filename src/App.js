554d38a0f6a81527a1abb40815dfb65a194fcccb
milanartis21 <132863051+milanartis21@users.noreply.github.com>
2024-05-08 19:49:34 -0600

    Added function search to be able search for the value keyword

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
+import Dictionary from "./Dictionary"
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header>
+      <div className="container">
+      <header className="App-header">
+        
         
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+Coded  by Milan
+</footer>
+
+    </div>
     </div>
   );
 }
 
-export default App;
+
