dd5986a32da2a2a6320d6e42ac389ae63989d0b0
milanartis21 <132863051+milanartis21@users.noreply.github.com>
2024-05-09 21:40:28 -0600

    rendering the meanings, added new components

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, {useState} from "react";
+import axios from "axios";
 import "./Dictionary.css";
-import axios from "./axios";
+import Results from "./Results";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState(" ");
+    let[results, setResults] = useState({});
 
     function handleResponse(response) {
-        console.log(response);
+        console.log(response.data[0]);
+        setResults(response.data[0]);
     }
    
     function search(event) {
@@ -24,5 +27,7 @@ export default function Dictionary() {
         <form onSubmit={search}>
             <input type="search" onChange={handleKeywordChange}/>
             </form>
+
+            <Results results= {results}/>
     </div>;
 }
