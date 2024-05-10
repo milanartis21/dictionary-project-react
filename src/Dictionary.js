0b88e8633fb6d71da7417d92b964677bbaaa2c87
milanartis21 <132863051+milanartis21@users.noreply.github.com>
2024-05-09 21:46:18 -0600

    updated source code

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,33 +1,34 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import "./Dictionary.css";
 import Results from "./Results";
+import "./Dictionary.css";
 
 export default function Dictionary() {
-    let [keyword, setKeyword] = useState(" ");
-    let[results, setResults] = useState({});
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
-    function handleResponse(response) {
-        console.log(response.data[0]);
-        setResults(response.data[0]);
-    }
-   
-    function search(event) {
-        event.preventDefault();
-        alert (`Searching for ${keyword} definition`);
+  function handleResponse(response) {
+    setResults(response.data[0]);
+  }
 
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        console.log(apiUrl);
-        axios.get(apiUrl).then(handleResponse);
-    }
-    function handleKeywordChange(event) {
-        setKeyword(event.target.value);
-    }
-    return  <div className="Dictionary">
-        <form onSubmit={search}>
-            <input type="search" onChange={handleKeywordChange}/>
-            </form>
+  function search(event) {
+    event.preventDefault();
 
-            <Results results= {results}/>
-    </div>;
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+      <Results results={results} />
+    </div>
+  );
 }
