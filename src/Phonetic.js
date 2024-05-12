b976e7a7e2fe7a2cd352e3c4e2c42778cd2f5e6c
milanartis21 <132863051+milanartis21@users.noreply.github.com>
2024-05-12 14:10:32 -0600

    FIxed a couple errors in synonyms file

---
+++ src/Phonetic.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function (props) {
+    console.log (props.phonetic);
+    return (
+        <div className="Phonetic">
+        <a href={props.phonetic.audio} target="_blank" >
+            Listen
+        </a>
+        <br />
+        {props.phonetic.text}
+        </div>
+    )
+
+
+}
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,19 +1,15 @@
 import React from "react";
 
 export default function Synonyms(props) {
-   if (props.synonyms) {
-    return ( 
-        <div className = "Synonyms">
-        {props.synonyms.map(function (synonyms, index) {
-            return 
-                <li key={index}>
-                {synonyms}
-                </li>;
+  if (props.synonyms) {
+    return (
+      <ul className="Synonyms">
+        {props.synonyms.map(function (synonym, index) {
+          return <li key={index}>{synonym}</li>;
         })}
-        </div>
+      </ul>
     );
-
-   } else {
+  } else {
     return null;
-   }
+  }
 }
